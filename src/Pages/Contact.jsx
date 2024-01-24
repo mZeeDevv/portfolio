711144bd29f99f6deda35c4571859377e5b06497
mZeeDev <hamzeekhattak56@gmail.com>
2024-01-24 21:33:44 +0500

    Added Netflity form

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Contact() {
   document.title = "mZeeDev | Contact"
+const [name, setName] = useState("")
+ const [email, setEmail] = useState("")
+ const [message, setMessage] = useState(" ")
+console.log(message)
+
   return (
     <>
       <div className='md:mx-16 flex flex-col my-3 mx-3'>
@@ -10,19 +15,30 @@ export default function Contact() {
         <div>
         <p className='max-w-5xl mx-auto'>Fill out the form below or shoot me an email.</p>
         <div className='max-w-5xl mx-auto md:px-10 md:py-10 py-3 px-3 shadow-md bg-white'>
-        <form className='' name='success' method='POST' data-netlify="true" onSubmit="submit" netlify>
+        <form  name='contact' method='POST'>
+          <input type="hidden" name="form-name" value="contact" />
           <div className='flex space-x-2' >
             <div className='md:w-[50%]'>
-            <label className='fontDes text-gray-500' for="text">Full Name <span className='font-semibold text-red-600'>*</span></label>
-            <input type="text" className='border-2 border-gray-300 rounded-md p-2 w-full' id='text' name='name'/>
+            <label className='fontDes text-gray-500' htmlFor="text">Full Name <span className='font-semibold text-red-600'>*</span></label>
+            <input type="text" className='border-2 border-gray-300 rounded-md p-2 w-full' id='text' name='name'
+            value={name} onChange={(e) => setName(e.target.value)}
+            />
             </div>
             <div className='md:w-[50%]'>
-            <label className='fontDes text-gray-500' for="email">Email <span className='font-semibold text-red-600'>*</span></label>
-            <input type="email" className='border-2 border-gray-300 rounded-md p-2 w-full' id='email' name='email'/>
+            <label className='fontDes text-gray-500' htmlFor="email">Email <span className='font-semibold text-red-600'>*</span></label>
+            <input type="email" className='border-2 border-gray-300 rounded-md p-2 w-full' id='email' name='email'
+            value={email} onChange={(e) => setEmail(e.target.value)}
+            />
             </div>
           </div>
-          <label className='fontDes text-gray-500 p-3' for="message">Message <span className='font-semibold text-red-600'>*</span></label>
-          <textarea className='border-2 border-gray-300 rounded-md p-2 w-full' name='message' id='message'></textarea>
+          <label className='fontDes text-gray-500 p-3' htmlFor="message">Message <span className='font-semibold text-red-600'>*</span></label>
+          <textarea
+                className='border-2 border-gray-300 rounded-md p-2 w-full'
+                name='message'
+                id='message'
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+              ></textarea>
           <button type='submit' className='font bg-gray-300 text-white p-3 rounded-sm'>Send message</button>
         </form>
         </div>
